7581c170a9f30585f3d3dccfff4dc959e713268a
sherryyu0823 <89732856+sherryyu0823@users.noreply.github.com>
2024-03-24 23:03:56 +0800

    Update video2gif.py

--- video2gif.py
+++ video2gif.py
@@ -3,20 +3,11 @@ import imageio
 import os
 from pathlib import Path
 
-
-# with open(os.path.join("./result2.0/DF", filename), 'r') as f:
-#     text = f.read()
-# for filename in os.listdir("./result2.0/FB"):
-#     print(filename)
-
-# def read_video(path):
-# frame_list = read_video('./result2.0/rlof_d1.mp4')
-# 		frame_to_gif(frame_list)
-
-for filename in os.listdir("./result2.0/DF"):
+# Convert the videos to gif in a folder
+for filename in os.listdir("Your Folder"):
 	# print(filename)
 	if(filename != 'gif'):
-		path = "./result2.0/DF/"+filename
+		path = "your folder/"+filename
 		print(path)
 		video_cap = cv2.VideoCapture(path)
 		frame_count = 0
@@ -25,7 +16,7 @@ for filename in os.listdir("./result2.0/DF"):
 			ret, frame = video_cap.read()
 			if ret is False:
 				break
-			frame = frame[..., ::-1]   # opencv读取BGR，转成RGB
+			frame = frame[..., ::-1]   # opencv BGR to RGB
 			all_frames.append(frame)
 			# cv2.imshow('frame', frame)
 			cv2.waitKey(1)
@@ -41,10 +32,4 @@ for filename in os.listdir("./result2.0/DF"):
 		file_name = Path(filename).stem
 		print(file_name)
 	# def frame_to_gif(frame_list):
-		gif = imageio.mimsave(("./result2.0/DF/gif/%s.gif" %file_name), all_frames, 'GIF', duration=0.00085)  
-	# duration 表示图片间隔
-
-
-# if __name__ == "__main__":
-	# frame_list = read_video('./result2.0/rlof_d1.mp4')
-	# frame_to_gif(frame_list)
+		gif = imageio.mimsave(("your folder/%s.gif" %file_name), all_frames, 'GIF', duration=0.00085)  
